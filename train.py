575fe14a99b0adf97144cd84190e0282666a41e7
Charles <adeakinyemi129@gmail.com>
2021-05-14 00:37:12 +0300

    commit

--- train.py
+++ train.py
@@ -1,14 +1,20 @@
 import json
+
+import torch.optim as optim
+from torch.optim.optimizer import Optimizer
+from torch.testing._internal.common_nn import output_size
+
 import nltk_utils
 
 #from PIL.TiffImagePlugin import idx
-from torch.testing._internal.distributed.rpc.examples.parameter_server_test import batch_size
+#from torch.testing._internal.distributed.rpc.examples.parameter_server_test import batch_size
 
 from nltk_utils import tokenize, stem, bag_of_words
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import NeuralNet
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -31,7 +37,7 @@ all_words = [stem(w) for w in all_words if w not in ignore_words]
 print(all_words)
 all_words = sorted(set(all_words))  #to remove duplicate element
 tags = sorted(set(tags))
-print(tags)
+
 
 X_train = []
 Y_train = []
@@ -63,6 +69,38 @@ class ChatDataset(Dataset):
 
 #Hyper parameters
 batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(X_train[0])
+learnin_rate = 0.001
+num_epochs = 1000
+
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset = dataset, batch_size= batch_size, shuffle=True, num_workers= 2)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+criterion = nn.CrossEntropyLoss()
+epitimizer = torch.optim.Adam(model.parameters(), lr= learnin_rate)
+
+for epoch in range(num_epochs):
+    for(words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+
+        #forward
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        #backward and optimizer step
+        Optimizer.zero_grad()
+        loss.backward()
+        Optimizer.step()
+
+    if(epoch + 1) % 100 == 0:
+        print(f'epoch {epoch + 1}/{num_epochs}, loss = {loss.item():,4f}')
+
+
+
